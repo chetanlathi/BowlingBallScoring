b8bfbd3a9eea7a8815fdaab3668b0eadd51ab4df
Chetan Lathi <chetan.lathi@ICERTIS.COM>
2021-09-20 21:36:52 +0530

    First Commit

---
+++ BowlingBallScoring/BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,138 @@
+﻿// Author : Chetan Lathi.   
+// Created Test Cases for BowlingBall Game
+// Created on : 20 September 2021
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        private Game _game=null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void Test_RollBall_With_AllZero()
+        {
+            //Arrange
+            RollBall(0, 20);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(0, actualResult);
+        }       
+
+        [TestMethod]
+        public void Test_RollBall_With_AllOne()
+        {
+            //Arrange
+            RollBall(1, 20);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(20, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_RollBall_With_OneSpare()
+        {
+            //Arrange
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+             RollBall(0, 17);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(16, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_OneStrike()
+        {
+            //Arrange
+            _game.Roll(10);
+            _game.Roll(3);
+            _game.Roll(4);
+            RollBall(0, 17);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(24, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_PerfectGame()
+        {
+            //Arrange
+            RollBall(10, 12);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(300, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_NoExtraRoll()
+        {
+            //Arrange
+            _game.Roll(new int[] { 4, 3, 8, 2, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 0 });
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(134, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_SpareThenStrikeAtEnd()
+        {
+            //Arrange
+            _game.Roll(new int[] { 4, 2, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 1, 10 });
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(145, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_ThreeStrikesAtEnd()
+        {
+            //Arrange
+            _game.Roll(new int[] { 5, 2, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 10, 10, 10 });
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(166, actualResult);
+        }
+
+        private void RollBall(int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ BowlingBallScoring/BowlingBall/Game.cs
@@ -0,0 +1,88 @@
+﻿// Author : Chetan Lathi.   
+// Created BowlingBall Game for Tavisca
+// Created on : 20 September 2021
+
+// Solution Details :
+//1. The game consists of 10 frames,In each frame the player has two opportunities to knock
+//   down 10 pins.
+//2. The GetScore method gives total socre which is calculated by the total number of pins knocked down,
+//   plus bonuses for strikes and spares.
+//3. A spare is when the player knocks down all 10 pins in two tries. The bonus for that frame is the number of pins
+//   knocked down by the next roll.
+//4. A strike is when the player knocks down all 10 pins on his first try. The bonus for that frame is the value of the
+//   next two balls rolled.
+//5. Assumtions : The solution should assume valid inputs at all times and does not need to include unnecessary validations
+//   trying to prevent bad input data.
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        private int[] _rolls = new int[21];
+        private int[] _frame = new int[10];
+        int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            _rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                _rolls[currentRoll++] = pins[i];
+            }
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int index = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isStrike(index))
+                {
+                    score += GetStrikeScore(index);
+                    index++;
+                }
+                else if (isSpare(index))
+                {
+                    score += GetSpareScore(index);
+                    index += 2;
+                }
+                else
+                {
+                    score += GetStandardScore(index);
+                    index += 2;
+                }
+            }
+            return score;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private bool isSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+        }
+
+        private int GetStrikeScore(int index)
+        {
+            return 10 + _rolls[index + 1] + _rolls[index + 2];           
+        }
+
+        private int GetSpareScore(int index)
+        {
+            return 10 + _rolls[index + 2];
+        }
+
+        private int GetStandardScore(int index)
+        {
+            return _rolls[index] + _rolls[index + 1];
+        }
+    }
+}
